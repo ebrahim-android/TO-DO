4c7eae27bf233021ec6817dfef792c86b7e2468f
Ebrahim Santana <ebrahimsantana35@gmail.com>
2025-12-02 16:17:39 -0400

    feat: new archive (TaskItem) to show the new screem

---
+++ app/src/main/java/com/example/to_do/view/TaskItem.kt
@@ -0,0 +1,4 @@
+package com.example.to_do.view
+
+class TaskItem {
+}
--- app/src/main/java/com/example/to_do/view/TaskScreem.kt
+++ app/src/main/java/com/example/to_do/view/TaskScreem.kt
@@ -3,6 +3,7 @@ package com.example.to_do.view
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
